895a9617e5934b891d750c69b4e4062b3e8b0b07
kobutomo <n.tomoro13@gmail.com>
2021-05-08 03:00:22 +0900

    Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,60 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@8192
+D=A
+@pixels
+M=D
+(REC)
+  @KBD
+  D=M
+  @FILLB
+  D;JGT
+  @REC
+  0;JMP
+(FILLB)
+  @i
+  M=0
+(LOOP1)
+  @i
+  D=M
+  @pixels
+  D=D-M
+  @WAIT
+  D;JGT
+  @i
+  D=M
+  @SCREEN
+  // ディスプレイのアドレスを変数に
+  A=A+D
+  M=-1
+  @i
+  M=M+1
+  @LOOP1
+  0;JMP
+(WAIT)
+  @KBD
+  D=M
+  @FILLW
+  D;JEQ
+  @WAIT
+  0;JMP
+(FILLW)
+  @i
+  M=0
+(LOOP2)
+  @i
+  D=M
+  @pixels
+  D=D-M
+  @REC
+  D;JGT
+  @i
+  D=M
+  @SCREEN
+  A=A+D
+  M=0
+  @i
+  M=M+1
+  @LOOP2
+  0;JMP
