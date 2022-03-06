32528083050bae5564c7ef14ec60a613aa6c2aeb
kobutomo <n.tomoro13@gmail.com>
2022-03-07 01:59:54 +0900

    8 章完了

---
+++ 08/FunctionCalls/SimpleFunction/SimpleFunction.asm
@@ -0,0 +1,166 @@
+// // This file is part of www.nand2tetris.org
+// // and the book "The Elements of Computing Systems"
+// // by Nisan and Schocken, MIT Press.
+// // File name: projects/08/FunctionCalls/SimpleFunction/SimpleFunction.vm
+// 
+// // Performs a simple calculation and returns the result.
+// function SimpleFunction.test 2
+(SimpleFunction.test)
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push local 0
+@0
+D=A
+@LCL
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push local 1
+@1
+D=A
+@LCL
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+@SP
+A=M
+M=D+M
+@SP
+M=M+1
+// not
+@SP
+M=M-1
+A=M
+D=M
+D=!D
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push argument 0
+@0
+D=A
+@ARG
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+@SP
+A=M
+M=D+M
+@SP
+M=M+1
+// push argument 1
+@1
+D=A
+@ARG
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+@SP
+A=M
+M=M-D
+@SP
+M=M+1
+// return
+@LCL
+D=M
+@R13
+M=D
+@5
+D=A
+@R13
+A=M-D
+D=M
+@R14
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@ARG
+A=M
+M=D
+D=A+1
+@SP
+M=D
+@1
+D=A
+@R13
+A=M-D
+D=M
+@THAT
+M=D
+@2
+D=A
+@R13
+A=M-D
+D=M
+@THIS
+M=D
+@3
+D=A
+@R13
+A=M-D
+D=M
+@ARG
+M=D
+@4
+D=A
+@R13
+A=M-D
+D=M
+@LCL
+M=D
+@R14
+A=M
+0;JMP
